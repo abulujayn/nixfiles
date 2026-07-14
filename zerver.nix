83772c1a55542c3cc64df7c6206858429e6a8b9c
abulujayn <zaeem@parkar.au>
2026-07-14 12:08:38 +1000

    enable netbootxyz for zerver

--- zerver.nix
+++ zerver.nix
@@ -2,6 +2,7 @@
 
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.netbootxyz.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = true;
