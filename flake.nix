aab9b8d8aed8fb3611fe3e4b5bae1a0bd8c142bf
abulujayn <zaeem@parkar.au>
2026-09-04 11:42:44 +1000

    remove x86_64-darwin from devshell

--- flake.nix
+++ flake.nix
@@ -96,7 +96,6 @@
       devShells = nixpkgs.lib.genAttrs [
         "aarch64-darwin"
         "aarch64-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ] (system: {
         default = nixpkgs.legacyPackages.${system}.mkShell {
