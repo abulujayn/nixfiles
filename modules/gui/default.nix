76477bf3bee540818b6c942e520f3e9a080d8a53
abulujayn <zaeem@parkar.au>
2026-09-08 12:16:07 +1000

    update noctalia scaling

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -126,6 +126,7 @@ in
   };
 
   environment.etc = {
+    "xdg/noctalia/00-accessibility.toml".source = ./config/noctalia/00-accessibility.toml;
     "xdg/noctalia/05-wallpaper.toml".source = ./config/noctalia/05-wallpaper.toml;
     "xdg/hypr/hyprland.lua" = {
       source = pkgs.replaceVars ./config/hypr/hyprland.lua {
@@ -167,6 +168,7 @@ in
   system.userFilesCleanup.${username} = [
     ".icons/default/index.theme"
     ".config/hypr/hyprland.lua"
+    ".config/noctalia/00-accessibility.toml"
     ".config/noctalia/05-wallpaper.toml"
     ".config/noctalia/10-theme.toml"
     ".config/noctalia/15-clock.toml"
